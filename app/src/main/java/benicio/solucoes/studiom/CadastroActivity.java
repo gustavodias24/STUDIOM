56c288fec155d3f5d969c36e51e2b087dd3562ca
gusta <gustavo024dias@gmail.com>
2024-01-06 15:53:17 -0300

    tela cadastro cliente

--- app/src/main/java/benicio/solucoes/studiom/CadastroActivity.java
+++ app/src/main/java/benicio/solucoes/studiom/CadastroActivity.java
@@ -4,15 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import benicio.solucoes.studiom.databinding.ActivityCadastroBinding;
+import benicio.solucoes.studiom.databinding.LayoutCadastroClienteBinding;
 
 public class CadastroActivity extends AppCompatActivity {
 
     private ActivityCadastroBinding mainBinding;
+    private Bundle bundle;
+    private Dialog dialogCadastro;
+    private int t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,27 @@ public class CadastroActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Cadastro");
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        bundle = getIntent().getExtras();
+        t = bundle.getInt("t", 1);
+
+        switch (t){
+            case 1:
+                configurarDialogCadastroCliente();
+                break;
+        }
+
+        mainBinding.fabCadastro.setOnClickListener(view -> dialogCadastro.show());
+
+
+    }
+
+    private void configurarDialogCadastroCliente(){
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+        b.setTitle("Cadastro de cliente");
+        LayoutCadastroClienteBinding cadastroClienteBinding = LayoutCadastroClienteBinding.inflate(getLayoutInflater());
+        b.setView(cadastroClienteBinding.getRoot());
+        dialogCadastro = b.create();
     }
 
     @Override
